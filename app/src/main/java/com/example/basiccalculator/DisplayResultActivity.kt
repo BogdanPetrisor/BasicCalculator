25f832cf0f50ee1c66e44716e681bace1e6bd286
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-14 12:18:59 +0300

    Create add function for "Add Button" functionality Modify TextViewResult from DisplayResultActivity to output the calculation

--- app/src/main/java/com/example/basiccalculator/DisplayResultActivity.kt
+++ app/src/main/java/com/example/basiccalculator/DisplayResultActivity.kt
@@ -2,10 +2,18 @@ package com.example.basiccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
+lateinit var operation : String
 class DisplayResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_result)
+        val result = intent.getStringExtra(operation)
+        val calculation = findViewById<TextView>(R.id.textViewResult).apply {
+            text = result
+        }
     }
+
+
 }
--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.basiccalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+const val ADD = "Add"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun add(view: View){
+        val firstNumber = findViewById<TextView>(R.id.textNumber1).text.toString().toInt()
+        val secondNumber = findViewById<TextView>(R.id.textNumber2).text.toString().toInt()
+        val result = (firstNumber + secondNumber).toString()
+        val intent = Intent(this,DisplayResultActivity::class.java).apply {
+            putExtra(ADD,result)
+            operation = ADD
+        }
+        startActivity(intent)
+    }
+
 }
