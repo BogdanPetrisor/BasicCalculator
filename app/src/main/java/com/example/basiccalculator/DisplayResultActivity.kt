98554e5aa0b0a1ebcffc42ada416b2e352012920
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-14 11:24:59 +0300

    Create a new activity to show the result of calculations

---
+++ app/src/main/java/com/example/basiccalculator/DisplayResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.basiccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_result)
+    }
+}
