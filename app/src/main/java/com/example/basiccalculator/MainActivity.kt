0c2b5f3c5ac7fd2a4729a6782baddbd6b4a01201
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-14 12:25:25 +0300

    Add functions for subtraction, multiplication, division

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 const val ADD = "Add"
+const val SUBTRACTION = "Subtraction"
+const val MULTIPLY = "Multiply"
+const val DIVISION = "Division"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +23,39 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this,DisplayResultActivity::class.java).apply {
             putExtra(ADD,result)
             operation = ADD
+        }
+        startActivity(intent)
+    }
+
+    fun subtraction(view: View){
+        val firstNumber = findViewById<TextView>(R.id.textNumber1).text.toString().toInt()
+        val secondNumber = findViewById<TextView>(R.id.textNumber2).text.toString().toInt()
+        val result = (firstNumber - secondNumber).toString()
+        val intent = Intent(this,DisplayResultActivity::class.java).apply {
+            putExtra(SUBTRACTION,result)
+            operation = SUBTRACTION
+        }
+        startActivity(intent)
+    }
+
+    fun multiply(view: View){
+        val firstNumber = findViewById<TextView>(R.id.textNumber1).text.toString().toInt()
+        val secondNumber = findViewById<TextView>(R.id.textNumber2).text.toString().toInt()
+        val result = (firstNumber * secondNumber).toString()
+        val intent = Intent(this,DisplayResultActivity::class.java).apply {
+            putExtra(MULTIPLY,result)
+            operation = MULTIPLY
+        }
+        startActivity(intent)
+    }
+
+    fun division(view: View){
+        val firstNumber = findViewById<TextView>(R.id.textNumber1).text.toString().toInt()
+        val secondNumber = findViewById<TextView>(R.id.textNumber2).text.toString().toInt()
+        val result = (firstNumber / secondNumber).toString()
+        val intent = Intent(this,DisplayResultActivity::class.java).apply {
+            putExtra(DIVISION,result)
+            operation = DIVISION
         }
         startActivity(intent)
     }
